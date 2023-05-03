703eae360aeccf51ed588283f33548a4ec9008c9
victoria parchevskaya <parchevskayavictoria@gmail.com>
2023-05-03 05:55:52 +0300

    feat: add dto and mappers

---
+++ src/main/java/ru/clevertec/ecl/model/dto/TagDto.java
@@ -0,0 +1,21 @@
+package ru.clevertec.ecl.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@EqualsAndHashCode(of = "name")
+public class TagDto {
+    private Long id;
+    private String name;
+}
